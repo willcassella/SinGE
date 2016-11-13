0fd0725960dc577299c0db73d66340a61e0112db
Will Cassella <wcassella@gmail.com>
2016-11-12 17:57:57 -0700

    Prevented multiple instances of the packages script from trying to download the same package

--- Scripts/packages.ps1
+++ Scripts/packages.ps1
@@ -4,18 +4,33 @@ $release = $args[1] # Package release to download from
 
 Write-Output "Scanning packages..."
 
+# Array of all dependent packages that are in progress by other instances
+$inProgress = New-Object System.Collections.ArrayList
+
 # Enumerate the list of packages
 foreach ($package in $args[2..$args.Length])
 {
     $packagePath = $packageDir + $package
+    $inProgressPath = $packagePath + ".in_progress"
 
-    # Check if we've already downloaded the package
-    if (Test-Path $packagePath)
+    # Check if The package is currently in progress
+    if (Test-Path $inProgressPath)
+    {
+        Write-Output ("Waiting on package {0} to finish." -f $package)
+        $inProgress.Add($inProgressPath)
+        continue
+    }
+    # Check if we've already installed the package
+    elseif (Test-Path $packagePath)
     {
         Write-Output ("Package {0} is up to date." -f $package)
         continue
     }
 
+    # Create the in-progress file
+    New-Item -Force $inProgressPath | Out-Null
+
+    # Get a temp directory to download to
     $temp = [System.IO.Path]::GetTempFileName() + ".zip"
 
     # Download the package
@@ -27,5 +42,21 @@ foreach ($package in $args[2..$args.Length])
     # Extract the package
     Write-Output ("Extracting package {0}..." -f $package)
     Expand-Archive $temp -dest $packagePath
+
+    # Delete the temporary file and the in-progress file
     Remove-Item $temp
+    Remove-Item $inProgressPath
+}
+
+# Wait for dependent packages to complete
+while ($inProgress.Count -gt 0)
+{
+    for ($i = 0; $i -lt $inProgress.Count; $i++)
+    {
+        # If the item is no longer downloading
+        if (-Not (Test-Path $inProgress[$i]))
+        {
+            $inProgress.RemoveAt($i)
+        }
+    }
 }
