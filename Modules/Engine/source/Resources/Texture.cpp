96f125a35a5de1b321f55a50caa06300b0073d70
Will Cassella <wcassella@gmail.com>
2017-02-13 10:14:36 -0700

    Fixed stupid issue with textures. I'm gonna have to put some time into the resource manager if I want to actually fix this

--- Modules/Engine/source/Resources/Texture.cpp
+++ Modules/Engine/source/Resources/Texture.cpp
@@ -2,6 +2,7 @@
 
 #include <Core/Reflection/ReflectionBuilder.h>
 #include <Core/Interfaces/IToArchive.h>
+#include <Core/Util/StringUtils.h>
 #include <Resource/Interfaces/IFromFile.h>
 #include "../../include/Engine/Resources/Texture.h"
 
@@ -18,13 +19,23 @@ namespace sge
 	}
 
     Texture::Texture(const std::string& path)
-        : _color_space(ColorSpace::S_RGB),
-        image(path)
+        : Texture()
 	{
+        from_file(path.c_str());
 	}
 
 	bool Texture::from_file(const char* path)
 	{
+        // TODO: Should have better method than this
+        if (string_ends_with(path, ".tif"))
+        {
+            _color_space = ColorSpace::RGB;
+        }
+        else
+        {
+            _color_space = ColorSpace::S_RGB;
+        }
+
 		return image.from_file(path);
 	}
 
